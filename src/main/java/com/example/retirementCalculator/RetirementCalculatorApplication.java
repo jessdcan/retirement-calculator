7f7a3ecd7dd0123f592cd71342e7fee88fcdded9
Jess Dworcan <jess.dworcan@entelect.co.za>
2025-05-09 10:33:43 +0200

    setup start

---
+++ src/main/java/com/example/retirementCalculator/RetirementCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.retirementCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetirementCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetirementCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/retirementCalculator/RetirementCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.retirementCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetirementCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
