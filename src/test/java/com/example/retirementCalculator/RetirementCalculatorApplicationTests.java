dc9d317cb34a1c7cef1c865fc98c627292f6b935
Jess Dworcan <jess.dworcan@entelect.co.za>
2025-05-13 09:56:44 +0200

    added test yml

--- src/test/java/com/example/retirementCalculator/RetirementCalculatorApplicationTests.java
+++ src/test/java/com/example/retirementCalculator/RetirementCalculatorApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.retirementCalculator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = RetirementCalculatorApplication.class)
 class RetirementCalculatorApplicationTests {
 
 	@Test
